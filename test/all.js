da654baa829c5a8d0e59a2e0106067e2f1db8898
Andrew Osheroff <andrewosh@gmail.com>
2020-08-05 14:31:32 +0200

    Added failing test

--- test/all.js
+++ test/all.js
@@ -27,7 +27,55 @@ tape('out of bounds iterator', async function (t) {
   })
 })
 
-function create () {
+tape('out of bounds iterator, string encoding', async function (t) {
+  const db = create({ keyEncoding: 'utf8' })
+
+  const b = db.batch()
+
+  await b.put('a', null)
+  await b.put('b', null)
+  await b.put('c', null)
+
+  await b.flush()
+
+  const s = db.createReadStream({ gte: 'f' })
+  let count = 0
+
+  s.on('data', function (data) {
+    count++
+  })
+
+  return new Promise(resolve => {
+    s.on('end', function () {
+      t.same(count, 0, 'no out of bounds reads')
+      resolve()
+    })
+  })
+})
+
+tape('out of bounds iterator, larger db', async function (t) {
+  const db = create({ keyEncoding: 'utf8' })
+
+  for (let i = 0; i < 100; i++) {
+    await db.put('' + i, 'hello world')
+  }
+
+  const s = db.createReadStream({ gte: 'a' })
+  let count = 0
+
+  s.on('data', function (data) {
+    count++
+  })
+
+  return new Promise(resolve => {
+    s.on('end', function () {
+      t.same(count, 0, 'no out of bounds reads')
+      resolve()
+    })
+  })
+})
+
+function create (opts) {
   const feed = require('hypercore')(require('random-access-memory'))
-  return new Hyperbee(feed)
+  return new Hyperbee(feed, opts)
 }

4400ad122c7cc4dbdc4d56e97f4217b186554957
Andrew Osheroff <andrewosh@gmail.com>
2020-08-06 20:07:07 +0200

    Add simple fuzzy test

--- test/all.js
+++ test/all.js
@@ -75,6 +75,58 @@ tape('out of bounds iterator, larger db', async function (t) {
   })
 })
 
+tape('test all short iterators', async function (t) {
+  const db = create({ keyEncoding: 'utf8' })
+
+  const SIZE = 8
+  const reference = []
+
+  for (let i = 0; i < SIZE; i++) {
+    const key = '' + i
+    await db.put(key, 'hello world')
+    reference.push(key)
+  }
+
+  const boundsOpts = [['gte', 'lte'], ['gte', 'lt'], ['gt', 'lte'], ['gt', 'lt']]
+  for (let i = 0; i < SIZE; i++) {
+    for (let j = 0; j < SIZE; j++) {
+      for (const [greater, lesser] of boundsOpts) {
+        const opts = { [greater]: '' + i, [lesser]: '' + j }
+        const entries = await collect(db.createReadStream(opts))
+        if (!validate(opts, entries)) {
+          return t.end()
+        }
+      }
+    }
+  }
+
+  function validate (opts, entries) {
+    const start = opts.gt ? reference.indexOf(opts.gt) + 1 : reference.indexOf(opts.gte)
+    const end = opts.lt ? reference.indexOf(opts.lt) : reference.indexOf(opts.lte) + 1
+    const range = reference.slice(start, end)
+    t.same(range.length, entries.length)
+    for (let i = 0; i < range.length; i++) {
+      if (!entries[i] || range[i] !== entries[i].key) {
+        console.log('FAILED WITH OPTS:', opts)
+        console.log('  range length:', range, 'start:', start, 'end:', end)
+        console.log('  entries length:', entries.length)
+        t.fail(`ranges did not match: expected ${range} got ${entries.map(e => e.key)}`)
+        return false
+      }
+    }
+    t.pass('range is ordered correctly')
+    return true
+  }
+})
+
+function collect (stream) {
+  return new Promise(resolve => {
+    const entries = []
+    stream.on('data', d => entries.push(d))
+    stream.on('end', () => resolve(entries))
+  })
+}
+
 function create (opts) {
   const feed = require('hypercore')(require('random-access-memory'))
   return new Hyperbee(feed, opts)

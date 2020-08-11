878a4344b80b017c319b52a3741fe6fd74feb062
Mathias Buus <mathiasbuus@gmail.com>
2020-08-11 13:05:32 +0200

    allow reverse history iteration

--- index.js
+++ index.js
@@ -478,8 +478,7 @@ class Batch {
 
     const stack = []
 
-    let root
-    let node = root = await this.getRoot()
+    let node = await this.getRoot()
     if (!node) return
 
     const seq = this.tree.feed.length + this.length
--- iterators/history.js
+++ iterators/history.js
@@ -3,16 +3,29 @@ module.exports = class HistoryIterator {
     this.db = db
     this.options = opts
     this.live = !!opts.live
-    this.since = opts.since || 1
+    this.since = opts.since || 0
     this.end = 0
+    this.reverse = !!opts.reverse
+    this.limit = typeof opts.limit === 'number' ? opts.limit : -1
   }
 
   async open () {
-    await this.db.ready()
+    await this.db.getRoot() // does the update dance
     this.end = this.live ? Infinity : this.db.version
+    if (this.since) return
+    if (this.reverse) this.since = this.end - 1
+    else this.since = 1
   }
 
   async next () {
+    if (this._limit === 0) return null
+    if (this._limit > 0) this._limit--
+
+    if (this.reverse) {
+      if (this.since < 1) return null
+      return this.db.getBlock(this.since--, this.options)
+    }
+
     if (this.since >= this.end) return null
     return this.db.getBlock(this.since++, this.options)
   }

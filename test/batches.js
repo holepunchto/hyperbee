33ac7476bb504534f27ba04132f1e874425955f3
Mathias Buus <mathiasbuus@gmail.com>
2020-09-01 14:42:40 +0200

    add batch tests, including locks

---
+++ test/batches.js
@@ -0,0 +1,79 @@
+const { create, collect } = require('./helpers')
+const tape = require('tape')
+
+tape('basic batch', async function (t) {
+  const db = create()
+
+  const b = db.batch()
+  await b.put('a', '1')
+  await b.put('b', '2')
+  await b.flush()
+
+  const all = await collect(db.createReadStream())
+
+  t.same(all, [
+    { seq: 1, key: 'a', value: '1' },
+    { seq: 2, key: 'b', value: '2' }
+  ])
+
+  t.end()
+})
+
+tape('batch overwriting itself', async function (t) {
+  const db = create()
+
+  const b = db.batch()
+  await b.put('a', '1')
+  await b.put('a', '2')
+  await b.flush()
+
+  const all = await collect(db.createReadStream())
+
+  t.same(all, [
+    { seq: 2, key: 'a', value: '2' }
+  ])
+
+  t.end()
+})
+
+tape('parallel batches', async function (t) {
+  const db = create()
+
+  const a = batch([
+    { key: 'a', value: '1' },
+    { key: 'b', value: '2' }
+  ])
+
+  const b = batch([
+    { key: 'a', value: '3' },
+    { key: 'b', value: '4' },
+    { key: 'c', value: '5' }
+  ])
+
+  const c = batch([
+    { key: 'b', value: '6' }
+  ])
+
+  await Promise.all([a, b, c])
+
+  const all = await collect(db.createReadStream())
+
+  t.same(all, [
+    { seq: 3, key: 'a', value: '3' },
+    { seq: 6, key: 'b', value: '6' },
+    { seq: 5, key: 'c', value: '5' }
+  ])
+
+  t.end()
+
+  async function batch (list) {
+    const b = db.batch()
+
+    for (const { type = 'put', key, value } of list) {
+      if (type === 'del') await b.del(key)
+      else await b.put(key, value)
+    }
+
+    return b.flush()
+  }
+})

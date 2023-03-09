93c01e6caaddd1dfa592f1f9a67ee5b9fc1d9e09
Lucas Barrena <lucasbarrena98@gmail.com>
2023-03-09 10:54:20 -0300

    maf feedback

--- index.js
+++ index.js
@@ -900,7 +900,7 @@ class Watcher extends EventEmitter {
     try {
       await this._run()
     } catch (err) {
-      this.emit('error', err)
+      if (!this.closed) this.emit('error', err)
       this.destroy()
     } finally {
       this.running = false
@@ -919,23 +919,11 @@ class Watcher extends EventEmitter {
         this.emit('change', snapshot.version, this.latestDiff)
         break
       }
-    } catch (err) {
-      if (this.closed) return
-      throw err
     } finally {
       this.stream = null
       this.latestDiff = snapshot.version
 
-      await this._closeSnapshot(snapshot)
-    }
-  }
-
-  async _closeSnapshot (snapshot) {
-    try {
       await snapshot.close()
-    } catch (err) {
-      if (this.closed) return
-      throw err
     }
   }
 

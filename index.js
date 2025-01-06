06b6d367cfb02483626aed14255d593eb9937aaf
Christophe Diederichs <chm-diederichs@protonmail.com>
2025-01-06 10:37:04 +0000

    use state properties instead of core

--- index.js
+++ index.js
@@ -145,20 +145,19 @@ class TreeNode {
   preload () {
     if (this.block === null) return
 
-    const core = getBackingCore(this.block.tree.core)
-    const indexedLength = getIndexedLength(this.block.tree.core)
-    const bitfield = core.core.bitfield
+    const core = this.block.tree.core
+    const indexedLength = this.block.tree.core.signedLength
 
     for (let i = 0; i < this.keys.length; i++) {
       const k = this.keys[i]
       if (k.value) continue
-      if (k.seq >= indexedLength || bitfield.get(k.seq)) continue
+      if (k.seq >= indexedLength) continue
       preloadBlock(core, k.seq)
     }
     for (let i = 0; i < this.children.length; i++) {
       const c = this.children[i]
       if (c.value) continue
-      if (c.seq >= indexedLength || bitfield.get(c.seq)) continue
+      if (c.seq >= indexedLength) continue
       preloadBlock(core, c.seq)
     }
   }
@@ -746,12 +745,12 @@ class Batch {
   }
 
   async _getNode (seq) {
-    const cached = this.tree._nodeCache !== null && this.core.fork === this.tree.core.fork ? this.tree._nodeCache.get(seq) : null
+    const cached = this.core.fork === this.tree.core.fork ? this.tree._nodeCache.get(seq) : null
     if (cached !== null) return cached
     const entry = await this.core.get(seq, { ...this.options, valueEncoding: Node })
     if (entry === null) throw BLOCK_NOT_AVAILABLE()
     const wrap = copyEntry(entry)
-    if (this.core.fork === this.tree.core.fork && this.tree._nodeCache !== null) this.tree._nodeCache.set(seq, wrap)
+    if (this.core.fork === this.tree.core.fork) this.tree._nodeCache.set(seq, wrap)
     return wrap
   }
 
@@ -1267,7 +1266,7 @@ class Watcher extends ReadyResource {
   _onappend () {
     // TODO: this is a light hack / fix for non-sparse session reporting .length's inside batches
     // the better solution is propably just to change non-sparse sessions to not report a fake length
-    if (!this.closing && !this.core.isAutobase && (!this.core.core || this.core.core.tree.length !== this.core.length)) return
+    if (!this.closing && !this.core.isAutobase && (!this.core.core || this.core.state.tree.length !== this.core.length)) return
 
     const resolve = this._resolveOnChange
     this._resolveOnChange = null
@@ -1593,18 +1592,6 @@ function copyEntry (entry) {
 
 function defaultDiffer (currentSnap, previousSnap, opts) {
   return currentSnap.createDiffStream(previousSnap, opts)
-}
-
-function getBackingCore (core) {
-  if (core._source) return core._source.originalCore
-  if (core.flush) return core.session
-  return core
-}
-
-function getIndexedLength (core) {
-  if (core._source) return core._source.core.indexedLength
-  if (core.flush) return core.indexedLength
-  return core.length
 }
 
 function sameValue (a, b) {
